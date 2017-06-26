4d87755092053c90a3b8933821b7192d3e088832
aleks <alekszalata@gmail.com>
2017-06-26 21:24:28 +0300

    Bullets

--- src/Game/Collusion.java
+++ src/Game/Collusion.java
@@ -13,6 +13,6 @@ public class Collusion {
 
     public static boolean checkShoot(float x1, float y1, float width1, float height1,
                                      float x2, float y2, float width2, float height2) {
-        return x1 + width1 * Bullet.r == x2 && x2 + width2 * Player.scale == x1 && y1 + height1 * Bullet.r == y2 && y2 + height2 * Player.scale == y1;
+        return x1 + Bullet.r * 2 > x2 && x2 + width2 * Player.scale > x1 && y1 + Bullet.r * 2  > y2 && y2 + height2 * Player.scale > y1;
     }
 }
